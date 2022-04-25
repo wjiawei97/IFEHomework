e3949f44076fd36caa2c09608d8142d9f72ee171
wjiawei97 <wjiawei97@gmail.com>
2022-04-25 20:18:02 +0800

    add accountBook

---
+++ test.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html> 
+<html> 
+<body> 
+
+<?php 
+$name = (isset($_POST['userName'])) ? $_POST['userName'] : 'no name';
+$computedString = "Hi, " . $name;
+$array = ['userName' => $name, 'computedString' => $computedString];
+echo json_encode($array);
+?> 
+
+</body> 
+</html>
